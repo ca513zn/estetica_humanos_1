b14a50c7481a6ec68ca14f4f9a30b26b662f9ee8
Carlos92Kwan <czuniga@kwantec.com>
2020-05-13 20:49:57 -0500

    Adding navbar

---
+++ src/apuntes.js
@@ -0,0 +1,33 @@
+/** 
+ * 
+//sintaxis - Arrow function
+const sumar = (miObjeto) => {
+    const numero1 = miObjeto.numero_1;
+    const numero2 = miObjeto.numero_2;
+    return numero1 + numero2;
+}
+
+//normal function de
+const restar = function() {
+
+};
+
+
+
+sumar({
+    numero_1: 1,
+    numero_2: 4
+});
+
+
+const alumno = ({
+    nombre: 'carlos',
+    calificacion: 90
+})
+
+ * */ 
+
+
+
+
+//Arrow function
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <div>Inicio</div>
+      <div>Tienda</div>
+      <div>Estilos Nuevos</div>
+      <div>Reseñas</div>
+    </div>
+  );
+};
+
+export default NavBar;
